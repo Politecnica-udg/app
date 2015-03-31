466c0d884935679dd823dabf05a8d665c2e63a87
root <root@noreply@politecnica.sems.udg.mx>
2015-03-30 22:38:44 -0600

    Revert "Cambio el manejo de url's"
    This reverts commit ed2ed7270305c0619bf9b7febe4bc4d54f0bc8c1.

--- main/url.php
+++ main/url.php
@@ -2,15 +2,15 @@
 	switch ($objects["poli"]->url_p(URL_short())) {
 		//App's generales
 		case '/':{$objects["poli"]->log_in();}break;
-		case 'distroy':{$objects["poli"]->distroy();}break;
-		case 'app_admin':{$objects["poli"]->app_admin();}break;
+		case '/distroy/':{$objects["poli"]->distroy();}break;
+		case '/app_admin/':{$objects["poli"]->app_admin();}break;
 		default:{$objects["poli"]->e404();}break;
 		//App's de maestro
-		case 'eval_grup':{$objects['maestro']->eval_grup();}break;
-		case 'pdf':{$objects['maestro']->pdf_M();}break;
+		case '/maestro/eval_grup/':{$objects['maestro']->eval_grup();}break;
+		case '/maestro/pdf/':{$objects['maestro']->pdf_M();}break;
 		//App's de alumno
-		case 'cali':{$objects['alumno']->cali();}break;
-		case 'alum_e':{$objects['alumno']->eval_A();}break;
-		case 'preguntas':{$objects['alumno']->preguntas();}break;
+		case '/alumno/cali/':{$objects['alumno']->cali();}break;
+		case '/alumno/alum_e/':{$objects['alumno']->eval_A();}break;
+		case '/alumno/preguntas/':{$objects['alumno']->preguntas();}break;
 	}
 ?>
