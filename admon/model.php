18bc6bd6722638369dc80d27ec25c9d96a55fa6a
normanatg <tonygtz51@gmail.com>
2015-04-22 01:00:48 -0500

    Modifico el admin del direc

--- admon/model.php
+++ admon/model.php
@@ -20,7 +20,7 @@
 			$query = $this->consulta("SELECT distinct ccar_prof,abr_car,gdo_prof,tur_prof,gpo_prof,casg_prof,nom_pes,cal_prof,cod_prof,capint_prof, cin FROM `asig_prof`
 				INNER JOIN carreras on ccar_car=ccar_prof
 				INNER JOIN planes on ccar_pes=ccar_prof AND gdo_pes=gdo_prof AND casg_pes=casg_prof
-				WHERE cal_prof='$conf_poli[cal_prof]' and cod_prof='$cod' and cin = '2';");
+				WHERE cal_prof='$conf_poli[cal_prof]' and cod_prof='$cod';");
 			if($this->numero_de_filas($query) > 0){
 				while ( $tsArray = $this->fetch_assoc($query) ) {
 					$data[] = $tsArray;
