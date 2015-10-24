a395b948f689aa8155422a703e4436259db52f32
normanatg <tonygtz51@gmail.com>
2015-10-24 16:55:21 -0500

    Cambios para no mostrar errores al enviar informacion

--- alumnos/controller.php
+++ alumnos/controller.php
@@ -4,9 +4,7 @@
 		function __construct(){$this->data = new general_Al();}
 		public function eval_A(){
 			global $conf_poli;
-			$mat[0] = $this->data->clases($conf_poli['cal_prof'][0]);
-			$mat[1] = $this->data->clases($conf_poli['cal_prof'][1]);
-			//$mat = array_merge($arr1,$arr2);
+			$mat = $this->data->clases($conf_poli['cal_prof']);
 			return render_to_response (vista_Al::gen_dinamic('lista.html',$mat));
 		}
 		public function cali()
