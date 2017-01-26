bcdc6e9adbd4c50dfe8e5b4d3db0e6829ea9a64a
Norman Torres <tonygtz51@gmail.com>
2017-01-26 17:46:40 -0600

    Cambios ultimo minuto11

--- assets/complementos/pdf.php
+++ assets/complementos/pdf.php
@@ -53,13 +53,8 @@
 			parent::__construct();
 		}
 		public function Header(){
+			$this->SetMargins(37,5);
 			$this->Image('assets/complementos/img/cabecera.jpg', 1, 1, 215, 35,'JPG');
-			$this->SetFont('Arial','I',12);
-			$this->Cell(27,10,"",0,2);
-			$this->Cell(27,5,"",0);
-			$this->Cell(50,5,'ESCUELA POLIT'.utf8_decode("É").'CNICA',0,2);
-			$this->Cell(55,5,"COORDINACION DE CARRERA",0,2);
-			$this->Cell(55,5,"VINCULACION",0,2);
 		}
 		public function Footer(){
 			$this->SetFont('Arial','I',7);
